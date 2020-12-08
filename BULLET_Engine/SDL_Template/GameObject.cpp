3c2e3cac6b52911da4f2e6120844019852e3e93d
iamdatyoung1 <45438168+iamdatyoung1@users.noreply.github.com>
2020-12-08 17:27:34 -0500

    Update GameObject.cpp

--- BULLET_Engine/SDL_Template/GameObject.cpp
+++ BULLET_Engine/SDL_Template/GameObject.cpp
@@ -1,8 +1,10 @@
 #include "GameObject.h"
 
+#include "MatrixMath.h"
+
 /* GameObject Constructor
-	Sets the GameObject's components to 0 */
-GameObject::GameObject() {
+	Sets the GameObject's components to 0. Also gets the model and normal matrix IDs from the shader for rendering. */
+GameObject::GameObject(Mesh* mesh_, Shader* shader_) : mesh(mesh_), shader(shader_) {
 	pos.x = 0;
 	pos.y = 0;
 	pos.z = 0;
@@ -13,11 +15,14 @@ GameObject::GameObject() {
 	accel.y = 0;
 	accel.z = 0;
 	mass = 0;
+
+	modelMatrixID = shader->getUniformID("modelMatrix");
+	normalMatrixID = shader->getUniformID("normalMatrix");
 }
 
 /* GameObject Constructor
-	Set the GameObject's components to the parameters supplied */
-GameObject::GameObject(Vec3 pos_, Vec3 vel_, Vec3 accel_, float mass_) {
+	Set the GameObject's components to the parameters supplied. Also gets the model and normal matrix IDs from the shader for rendering. */
+GameObject::GameObject(Vec3 pos_, Vec3 vel_, Vec3 accel_, float mass_, Mesh* mesh_, Shader* shader_) : mesh(mesh_), shader(shader_) {
 	pos.x = pos_.x;
 	pos.y = pos_.y;
 	pos.z = pos_.z;
@@ -28,13 +33,16 @@ GameObject::GameObject(Vec3 pos_, Vec3 vel_, Vec3 accel_, float mass_) {
 	accel.y = accel_.y;
 	accel.z = accel_.z;
 	mass = mass_;
+
+	modelMatrixID = shader->getUniformID("modelMatrix");
+	normalMatrixID = shader->getUniformID("normalMatrix");
 }
 
 /* GameObject Deconstructor */
 GameObject::~GameObject() {
 }
 
-/* Update function 
+/* Update function
 	Updates the GameObject's position using it's current velocity and acceleration */
 void GameObject::Update(const float deltaTime) {
 	pos.x += vel.x * deltaTime + 0.5f * accel.x * deltaTime * deltaTime;
@@ -45,6 +53,20 @@ void GameObject::Update(const float deltaTime) {
 	vel.z += accel.z * deltaTime;
 }
 
+void GameObject::Render()
+{
+	Matrix3 normalMatrix = MatrixMath::transpose(MatrixMath::inverse(modelMatrix));
+
+	glUniformMatrix4fv(modelMatrixID, 1, GL_FALSE, modelMatrix.matrix4);
+	glUniformMatrix3fv(normalMatrixID, 1, GL_FALSE, normalMatrix.matrix3);
+	
+	mesh->Render();
+}
+
+void GameObject::OnDestroy()
+{
+}
+
 /* Apply Force function
 	Calculates the GameObject's new acceleration with the given applied force and it's mass */
 void GameObject::ApplyForce(Vec3 force) {
@@ -53,7 +75,7 @@ void GameObject::ApplyForce(Vec3 force) {
 	accel.z = force.z / mass;
 }
 
-/* Get Position Function 
+/* Get Position Function
 	Gets the GameObject's current position */
 Vec3 GameObject::GetPos() {
 	return pos;
